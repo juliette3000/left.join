39358cbfe9914247fe57ed1fee7d5b7e5389f108
Tophe <Tophe@hj-PC>
2019-10-13 19:04:18 +0200

    rhhaaa

---
+++ sql/create_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS `osiris`.`Resident` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
---
+++ sql/insert_resident.sql
@@ -0,0 +1,2 @@
+INSERT INTO `resident` (`id`, `nom`) VALUES ('17', 'juliette');
+INSERT INTO `resident` (`id`, `nom`) VALUES ('800', 'yann');
