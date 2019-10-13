de74e924a22779df2f288c7e674066c8d04fe000
Tophe <Tophe@hj-PC>
2019-10-13 19:49:23 +0200

    connexion SQL avec version PHP 7 (et 5 en commentaire)

--- traitement.php
+++ traitement.php
@@ -4,28 +4,44 @@ if (isset($_POST['lhss']))
 {
 	$param_lhss = $_POST['lhss'];
 }
-else {
-	echo "param [lhss] manquant";
-	exit;
-}
-if (empty($param_lhss)){
-	
-	echo "param [lhss] est vide ";
-	exit;
-}
 
 
 	include 'constantes.php';
 
+
 	// Create connection http://php.net/manual/fr/function.mysql-connect.php
-	
-	$link  =  mysql_connect($servername, $username, $password)
-	or die( "Impossible de se connecter : "  .  mysql_error ());
-	
-	mysql_select_db($dbname);
-	
-	
-	$sql = "SELECT annee as 
 
+	
+	// 	$link  =  mysql_connect($servername, $username, $password) or die( "Impossible de se connecter : "  .  mysql_error ()); // version 5
+	
+	
+	$conn = new mysqli($servername, $username, $password, $dbname); // php version 7 en local
+	// Check connection
+	if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
+	
+	$sql = "SELECT id, nom FROM resident";
 
+	
+	
+	$result = $conn->query($sql);
+	
+	// if ($result->num_rows > 0) {
+	
+	// OLD if ( $nb == 0 ) {
+	if ($result->num_rows == -1) {
+		// rien à faire
+	}
+	else {
+		// on fait une boucle qui va faire un tour pour chaque enregistrement
+		// OLD while($row = mysql_fetch_assoc($req)){
+		while($row = $result->fetch_assoc()) {
+				echo $row["id"] . ' -  nom : ' . $row["nom"] ;
+
+		}
+	}
+	
+	
+	
+	
+	
 ?>
