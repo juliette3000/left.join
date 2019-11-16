7ffe034bd639ff9c007e136fa71e0e591b789071
dell <dell@PC-de-dell>
2019-11-16 20:44:36 +0100

    exclude dans copy.bat + requierd form + getCim10.php

--- www/enquete_lam_2019/getCim10.php
+++ www/enquete_lam_2019/getCim10.php
@@ -1,24 +1,27 @@
 <?php
-
 include "constantes.php";
 
-if(isset($_POST['search'])){
- $search = $_POST['search'];
+if (isset ( $_GET ['search'] )) {
+	$search = $_GET ['search'];
+	
+	$query = "SELECT * FROM Cim10 WHERE name like '%" . $search . "%'";
+	
+	$conn = new mysqli ( $servername, $username, $password, $dbname );
+	
+	$result = mysqli_query ( $conn, $query );
+	
+	$response = array ();
+	while ( $row = mysqli_fetch_array ( $result ) ) {
+		echo  $row ['code'] . $row ['name'] ;
+	}
+	
 
- $query = "SELECT * FROM Cim10 WHERE nom like'%".$search."%'";
- 
- $conn = new mysqli($servername, $username, $password, $dbname);
- 
- $result = mysqli_query($conn, $query);
-
- $response = array();
- while($row = mysqli_fetch_array($result) ){
-   $response[] = array("code"=>$row['code'],"nom"=>$row['nom']);
- }
-
- echo json_encode($response);
+	
+} else {
+	
+	echo 'blaireau y a pas de search';
 }
 
-exit;
+exit ();
 
 ?>
--- www/enquete_lam_2019/traitement_confirmation.php
+++ www/enquete_lam_2019/traitement_confirmation.php
@@ -120,11 +120,30 @@ if (isset($_POST['sexe'])
 else {echo "erreur";}
 
 
+include 'constantes.php';
+// Create connection http://php.net/manual/fr/function.mysql-connect.php
+
+// 	$link  =  mysql_connect($servername, $username, $password) or die( "Impossible de se connecter : "  .  mysql_error ()); // version 5
+
+
+$conn = new mysqli($servername, $username, $password, $dbname); // php version 7 en local
+// Check connection
+if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
+
 $sql = "INSERT INTO enquete (sexe,	annee_naissance, date_entree,	mode_admission,	mode_admission_autre, 	statut_admin, 	couv_sociale, couv_socialec, aah,	pch,	invalidite, accord_mdph_fam, accord_mdph_savs, accord_mdph_samsah,	aide_soc_fam,		aide_soc_savs,		aide_soc_samsah,	departement_mdph, situation_recherche_place, date_demande, nombre_refus, derogation_age_ehpad, aide_soc_ehpad, situation_place_ehpad, protection_juridique, protection_juridique_precision) 
 VALUES 					('$sexe',	'$datenaiss',	'$dateentree',	'$mode_entree', '$autreentree',			'$droits',		'$couv',	'$couvc',		'$aah','$pch',	'$inv',		'$mdph',		'$mdphsavs',		'$mdphsams',		'$aidesocialefam',	'$aidesocialesavs',	'$aidesocialesams','$departementmdph','$precisionmdph',			'$precisionmdphbis',		'$precisionmdphter','$ehpad ',		'$aidesocialeehpad',		'$placeehpad',		'$protection',	'$precisionprotection' )";
 
 
-echo " Etes vous sûr de :" . $sql  . " ? ";
+if ($conn->query($sql) === TRUE)
+{	echo $datenaiss. " a bien été enregistré !" ;
+}
+
+
+
+else {
+	echo "Error: " . $sql . "<br>" . $conn->error;}
+	
+	$conn->close();
 	
 
 ?>
