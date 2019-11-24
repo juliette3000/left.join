98836bb07bcdb5ac4030daf67b1318cdc3305eb1
dell <dell@PC-de-dell>
2019-11-24 19:13:26 +0100

    page confirmation avant INSERT : affichage tableau HTML

--- www/enquete_lam_2019/traitement.php
+++ www/enquete_lam_2019/traitement.php
@@ -11,6 +11,7 @@ if (isset($_POST['autreentree'])) {
 }
 	
 	
+
 	
 if (isset($_POST['departementmdph'])) {
 	$departementmdph = $_POST['departementmdph'];
@@ -21,6 +22,33 @@ if (isset($_POST['departementmdph'])) {
 
 	
 }
+
+$sexe = $_POST['sexe'];
+	$datenaiss = $_POST['datenaiss'];
+	$dateentree = $_POST['dateentree'];
+	$mode_entree = $_POST['mode_entree'];
+	$droits = $_POST['droits'];
+	$couv = $_POST['couv'];
+	$couvc = $_POST['couvc'];
+	$aah = $_POST['aah'];
+	$pch = $_POST['pch'];
+	$inv = $_POST['inv'];
+	$mdph = $_POST['mdph'];
+	$mdphsavs = $_POST['mdphsavs'];
+	$mdphsams = $_POST['mdphsams'];
+	$aidesocialefam = $_POST['aidesocialefam'];
+	$aidesocialesavs = $_POST['aidesocialesavs'];
+	$aidesocialesams = $_POST['aidesocialesams'];
+	
+	$precisionmdph = $_POST['precisionmdph'];
+	$precisionmdphbis = $_POST['precisionmdphbis'];
+	$precisionmdphter = $_POST['precisionmdphter'];
+	$ehpad = $_POST['ehpad'];
+	$aidesocialeehpad = $_POST['aidesocialeehpad'];
+	$placeehpad = $_POST['placeehpad'];
+	$protection = $_POST['protection'];
+	$precisionprotection = $_POST['precisionprotection'];
+	
 
 if (isset($_POST['sexe'])
 		&& isset($_POST['datenaiss'])
@@ -81,38 +109,6 @@ if (isset($_POST['sexe'])
 	$precisionprotection = $_POST['precisionprotection'];
 	
 
-	
-	
-	echo $datenaiss. " a bien été enregistré !" ;	
-	echo $sexe;
-	echo $dateentree;
-	echo $mode_entree;
-	echo $autreentree;
-	echo $droits;
-	echo $couv;
-	echo $couvc;
-	echo $aah;
-	echo $pch;
-	echo $inv;
-	echo $mdph;
-	echo $mdphsavs;
-	echo $mdphsams;
-	//echo $aidesocialesams;
-	//echo $aidesocialefam;
-	//echo $aidesocialesavs;
-	//echo $departementmdph;
-	//echo $precisionmdph;
-	//echo $precisionmdphbis;
-	//echo $precisionmdphter;
-	//echo $ehpad;
-	//echo $aidesocialeehpad;
-	//echo $placeehpad;
-	//echo $protection;
-	//echo $precisionprotection;
-	
-	
-	
-	
 }
 
 
@@ -120,11 +116,13 @@ if (isset($_POST['sexe'])
 else {echo "erreur";}
 
 
-$sql = "INSERT INTO enquete (sexe,	annee_naissance, date_entree,	mode_admission,	mode_admission_autre, 	statut_admin, 	couv_sociale, couv_socialec, aah,	pch,	invalidite, accord_mdph_fam, accord_mdph_savs, accord_mdph_samsah,	aide_soc_fam,		aide_soc_savs,		aide_soc_samsah,	departement_mdph, situation_recherche_place, date_demande, nombre_refus, derogation_age_ehpad, aide_soc_ehpad, situation_place_ehpad, protection_juridique, protection_juridique_precision) 
-VALUES 					('$sexe',	'$datenaiss',	'$dateentree',	'$mode_entree', '$autreentree',			'$droits',		'$couv',	'$couvc',		'$aah','$pch',	'$inv',		'$mdph',		'$mdphsavs',		'$mdphsams',		'$aidesocialefam',	'$aidesocialesavs',	'$aidesocialesams','$departementmdph','$precisionmdph',			'$precisionmdphbis',		'$precisionmdphter','$ehpad ',		'$aidesocialeehpad',		'$placeehpad',		'$protection',	'$precisionprotection' )";
+$sql = "<table><tr><td>sexe</td><td> $sexe </td></tr>
+<tr><td>annee_naissance<tr><td>date_entree</td></tr>
+<td>$dateentree/tr><tr><td>mode_admission,	mode_admission_autre, 	statut_admin, 	couv_sociale, couv_socialec, aah,	pch,	invalidite, accord_mdph_fam, accord_mdph_savs, accord_mdph_samsah,	aide_soc_fam<tr><td>	aide_soc_savs,		aide_soc_samsah,	departement_mdph, situation_recherche_place, date_demande, nombre_refus, derogation_age_ehpad, aide_soc_ehpad, situation_place_ehpad, protection_juridique, protection_juridique_precision) 
+VALUES 					('',	'$datenaiss',	,	'$mode_entree', '$autreentree',			'$droits',		'$couv',	'$couvc',		'$aah','$pch',	'$inv',		'$mdph',		'$mdphsavs',		'$mdphsams',		'$aidesocialefam',	'$aidesocialesavs',	'$aidesocialesams','$departementmdph','$precisionmdph',			'$precisionmdphbis',		'$precisionmdphter','$ehpad ',		'$aidesocialeehpad',		'$placeehpad',		'$protection',	'$precisionprotection' )";
 
 
-echo " Etes vous sûr de :" . $sql  . " ? ";
+echo " Etes vous sûr d enregistrer ces données :" . $sql  . " ? ";
 	
 
 ?>
