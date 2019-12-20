ef25b88b23ad27597e47148fcb6031bbabc5065d
yabd-el-kader <yabd-el-kader@atih.sante.fr>
2019-12-20 17:11:27 +0100

    getCim10_free (old PHP)

---
+++ www/enquete_lam_2019/getCim10_free.php
@@ -0,0 +1,46 @@
+<?php
+include "constantes.php";
+
+if (isset ( $_GET ['search'] )) {
+    $search = $_GET ['search'];
+    
+    $sql = "SELECT * FROM Cim10 WHERE name like '%" . $search . "%'";
+    
+    $link  =  mysql_connect($servername, $username, $password) or die( "Impossible de se connecter : "  .  mysql_error ());
+    
+    mysql_select_db($dbname);
+
+    $req = mysql_query($sql) or die("['Erreur SQL !','" .$sql. "','" . mysql_error() . "]");
+    
+    $nb = mysql_num_rows($req);
+
+    echo '[';
+    
+    if ( $nb == 0 ) {
+        // rien à faire
+        
+    } else {
+        // on fait une boucle qui va faire un tour pour chaque enregistrement
+        $isFirst = true;
+        while($row = mysql_fetch_assoc($req)){
+     // while ( $row = mysqli_fetch_array ( $result ) ) {
+            if ($isFirst){
+                echo  '{"id":"' . $row ['code'] . '","tagName":"' . $row ['name'] . ' ('. $row ['code'] . ')"}';
+                $isFirst = false;
+            }
+            else {
+                echo  ',{"id":"' . $row ['code'] . '","tagName":"' . $row ['name'] . ' ('. $row ['code'] . ')"}';
+            }
+        }
+    }
+    
+    echo ']';
+    
+} else {
+    
+    echo 'blaireau y a pas de search';
+}
+
+exit ();
+
+?>
--- www/enquete_lam_2019/index.php
+++ www/enquete_lam_2019/index.php
@@ -2,7 +2,7 @@
     <head>
         <meta charset="utf-8">
         <!-- importer le fichier de style -->
-        <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+        <link rel="stylesheet" href="css/login.css" media="screen" type="text/css" />
     </head>
     <body style='background:#fff;'>
         <div id="content">
